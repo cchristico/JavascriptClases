bb3845562f07566f593513da44daa930ea854933
Christian Castillo <cchristico@gmail.com>
2016-11-30 14:38:59 -0500

    Listar usuario y usuarios

--- servidorHTTP/app.js
+++ servidorHTTP/app.js
@@ -1,17 +1,51 @@
 var express = require('express');
 var app = express();
 var puerto=5050;
+var usuario=[
+    {
+        id:1,
+        nombre:'jose',
+        nombre:'123124'
+    },
+    {
+        id:2,
+        nombre:'marco',
+        nombre:'234324'
+    },
+    {
+        id:3,
+        nombre:'julio',
+        nombre:'0978888'
+    }
+    
+]
 
 app.get('/TecnologiasWeb', function (req, res) {
   res.send('Hello World!');
 });
+
+app.get('/Usuarios', function (req, res) {
+    res.json(usuario)
+})
+app.get('/Usuario/:idUsuario', function (req, res) {
+    var idActual=req.params.idUsuario;
+    for(var i=0;i<usuario.length;i++)
+        {
+            if(idActual==usuario[i].id)
+                {
+                res.json(usuario[i]);    
+                }
+                
+                
+                
+        }
+    res.json('Ya nada');        
+    
+});
+
 // This responds a POST request for the homepage
 app.post('/TecnologiasWeb/Post', function (req, res) {
-    /*objeto json*/
-    var usuario={
-        nombre:'José',
-        cedula:'123'
-    }   
+    
     res.append('token','123');
     usuario.apellido='peres'
     usuario.mascotas=[]
