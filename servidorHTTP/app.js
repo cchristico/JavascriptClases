00ea9efc48c2cff62046521812a87cdb21849285
Christian Castillo <cchristico@gmail.com>
2016-11-30 14:18:47 -0500

    envio de json y valores en header

--- servidorHTTP/app.js
+++ servidorHTTP/app.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var app = express();
+var puerto=5050;
 
 app.get('/TecnologiasWeb', function (req, res) {
   res.send('Hello World!');
 });
 // This responds a POST request for the homepage
-app.post('/Post', function (req, res) {
-   console.log("Got a POST request for the homepage");
-   res.send('Hello POST');
+app.post('/TecnologiasWeb/Post', function (req, res) {
+    /*objeto json*/
+    var usuario={
+        nombre:'José',
+        cedula:'123'
+    }   
+    res.append('token','123');
+    usuario.apellido='peres'
+    usuario.mascotas=[]
+   console.log('lo que tengo en la cabecera request'+req.headers);
+    console.log('lo que tengo en la cabecera response'+res.headers);
+//    res.send('Hello POST');
+       res.json(usuario);
+    
 });
 
-app.listen(5050, function () {
-  console.log('Ejemplo de escucha por el puerto 5050!');
+app.listen(puerto, function () {
+  console.log('Ejemplo de escucha por el puerto: '+puerto+'!');
 });
