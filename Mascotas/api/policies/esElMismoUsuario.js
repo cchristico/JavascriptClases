816bbae790306366aaa357392f6a50a975aff30f
Christian Castillo <christian.castillo@epn.edu.ec>
2017-02-23 14:49:06 -0500

    reglas

---
+++ Mascotas/api/policies/esElMismoUsuario.js
@@ -0,0 +1,9 @@
+module.exports = function(req, res, next) {
+
+    var parametros = req.allParams();
+    if(parametros.id==req.session.id)
+        {
+            return next();
+        }
+  return res.forbidden('Usted no puede editar');
+};
